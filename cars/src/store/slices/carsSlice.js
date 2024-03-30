dbadff59bca0867468ed8b6ee250e21b49ac8e25
Dylan White <white.dylan@live.co.uk>
2024-03-30 17:15:06 +0000

    More on redux toolkit

---
+++ cars/src/store/slices/carsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTerm: '',
+        data: []
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            state.searchTerm = action.payload
+        },
+        addCar(state, action) {
+            // Assumption:
+            // action.payload === { name: 'ab', cost: 140 }
+            state.data.push({
+                id: nanoid(),
+                name: action.payload.name,
+                cost: action.payload.cost
+            })
+        },
+        removeCar(state, action) {
+            // Assumption:
+            // action.payload === id of car to remove
+            const updated = state.data.filter((car) => {
+                return car.id !== action.payload
+            })
+            state.data = updated
+        }
+    }
+})
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions
+export const carsReducer = carsSlice.reducer
